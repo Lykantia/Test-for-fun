79f7ef6f853fbdd36aafdcbec02883eea14aff54
Lykantia <132667571+Lykantia@users.noreply.github.com>
2023-12-27 18:38:31 +0100

    Update Wowhead.cy.js
    Added pick DK class and random choosing of another class

--- TestingForFun/cypress/e2e/Wowhead.cy.js
+++ TestingForFun/cypress/e2e/Wowhead.cy.js
@@ -1,38 +1,50 @@
 describe ('Exploring Wowhead', () => {
     it('visit', () =>{
+      cy.viewport(1200, 800)
         cy.log('visit home page');
         cy.visit ('https://www.wowhead.com/wow'); 
+        cy.url().should('include', '/wow');
 
-        cy.getCookie('#onetrust-group-container', { timeout: 10000 })//.then((cookie) => {//cy.getCookie('.ot-sdk-row', { timeout: 10000 }).then((cookie) => {
-        //cy.getCookie('weCareAboutYourPrivacy', { timeout: 10000 }).then((cookie) => {
-            //if (cookie) {
-            //cy.log(`Cookie Content: ${cookie.value}`);
+        cy.get('#onetrust-group-container', { timeout: 10000 }).should('be.visible').then((cookie) => {
+            if (cookie) {
             cy.get('#onetrust-reject-all-handler').click();
-            //} else {
+            } else {
 
-           // cy.log('Cookie not found within the specified timeout');
-           // }
-    //});
-        cy.log('click on classes on main page')
+            cy.log('Cookie not found within the specified timeout');
+        }
+    });
 
-        //hub-img
-        
-        cy.get('.header-nav-classes').click()
+    cy.get('.header-nav-classes > span').should('be.visible').should('contain', 'Classes').click();
+    
+    cy.log('The classes on classes page');
 
-        cy.get('.class-list clear').its('length').should('be.gte', 1).then((len) => {
+    cy.get('.heading-size-1 > .c6').should('be.visible').should('contain', 'Death Knight').click();
 
-            const randomIndex = Cypress._.random(0, len - 1);
-        
-            // Click on the randomly selected list item
-            cy.get('.list-item').eq(randomIndex).should('be.visible').click();
-        });
-        //Cy.get('.dropdown-item').its('length').then((len) => {
-        //const randomIndex = Cypress._.random(0, len - 1);
+    cy.log('The DK class');
+    cy.url().should('include', '/death-knight');
+    cy.get('h1', { timeout: 10000 }).should('be.visible').should('contain', 'Death Knight Guides');
+    cy.get('.heading-size-2').should('be.visible').should('contain', 'Class Overview');
+    cy.get('.heading-size-2').should('be.visible').should('contain', 'Talent Trees');
+    cy.get('.heading-size-2').should('be.visible').should('contain', 'Rotation');
+    
+    cy.get('.guide-list-featured-guides-links').should('be.visible').should('contain', 'Blood Death Knight', 'Frost Death Knight', 'Unholy Death Knight');
 
-  // Click on the randomly selected item
-//cy.get('.dropdown-item').eq(randomIndex).click();
-//});
-        //cy.get('.tile-list-item').should('have.length.greater.then', 0)
-//*/
-})
+    cy.log('The choosing of random class and assertion');
+    cy.get('.listview-band-top > .listview-note').should('be.visible')
+
+    const randomIndex = Math.floor(Math.random() * 13) + 1;
+
+    const selector = `.listview-note > :nth-child(${randomIndex}) > a`;
+
+    cy.get(selector).click();
+    cy.contains('span', 'All').click(); 
+    
+    cy.get(selector).invoke('text').then((selectedElementText) => {
+
+      const expectedUrlPart = selectedElementText.trim().toLowerCase();
+
+      cy.url().should('include', expectedUrlPart);
+
+      })
+  })
 })
